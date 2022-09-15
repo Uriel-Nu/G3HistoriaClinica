59d02c0058ede30652748b557c43cc690430e9b1
Candelaria Pilar Cernadas <candelariapilarcernadas1998@hotmail.com>
2022-09-15 11:29:19 -0300

    Propiedad Estado_Civil

--- HistoriaClinicaBD/Data/Entidades/HistoriaClinica.cs
+++ HistoriaClinicaBD/Data/Entidades/HistoriaClinica.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace HistoriaClinicaBD.Data.Entidades
 {
+
     public class HistoriaClinica
     {
+
+        [Required(ErrorMessage = " Campo obligatorio")]
+        [MaxLength(10, ErrorMessage = " El Estado_Civil, es obligatorio ")]
+        public string Estado_Civil { get; set; }
+
+
     }
+
 }
