1f970469e62b48a59df438b75fb0e4191f40000d
UsuarioUsuario@DESKTOP-2NT6HAM <urielalberto75@hotmail.com>
2022-09-15 08:25:04 -0300

    Agregar archivos de proyecto.

---
+++ HistoriaClinicaBD/Data/Entidades/HistoriaClinica.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HistoriaClinicaBD.Data.Entidades
+{
+    public class HistoriaClinica
+    {
+    }
+}
