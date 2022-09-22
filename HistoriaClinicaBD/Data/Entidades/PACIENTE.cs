6aaecdd9959ec6b63a089bf3057992697d2d3efe
UsuarioUsuario@DESKTOP-2NT6HAM <urielalberto75@hotmail.com>
2022-09-21 22:03:08 -0300

    comprobacion

---
+++ HistoriaClinicaBD/Data/Entidades/PACIENTE.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HistoriaClinicaBD.Data.Entidades
+{
+    public class PACIENTE : EntytyBase
+    {
+        [Required(ErrorMessage = " Campo obligatorio")]
+        [MaxLength(10, ErrorMessage = " El nombre, es obligatorio ")]
+
+        public string nombre{ get; set; }
+
+        [Required(ErrorMessage = " Campo obligatorio")]
+        [MaxLength(10, ErrorMessage = " El nombre, es obligatorio ")]
+
+        public string Apellido { get; set; }
+
+        [Required(ErrorMessage = " Campo obligatorio")]
+        [MaxLength(10, ErrorMessage = " El nombre, es obligatorio ")]
+        public string DNI { get; set; }
+
+        [Required(ErrorMessage = " Campo obligatorio")]
+        [MaxLength(10, ErrorMessage = " El nombre, es obligatorio ")]
+        public string Direccion { get; set; }
+
+        [Required(ErrorMessage = " Campo obligatorio")]
+        
+        public int telefono { get; set; }
+        // esta finado si o no
+    }
+}
