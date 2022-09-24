dcf6492932f276b1d4966f3bf314d753d23ad0e8
UsuarioUsuario@DESKTOP-2NT6HAM <urielalberto75@hotmail.com>
2022-09-24 16:00:40 -0300

    mas metodo get,post

---
+++ G3HistoriaClinica/Server/Controllers/PacientesController.cs
@@ -0,0 +1,52 @@
+﻿using HistoriaClinicaBD.Data;
+using HistoriaClinicaBD.Data.Entidades;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace G3HistoriaClinica.Server.Controllers
+{
+    [ApiController]
+    [Route("Api/Pacientes")]
+
+    public class PacientesController : ControllerBase
+    {
+        private readonly Conbd inydbcont;
+
+        public PacientesController(Conbd Inydbcont)
+        {
+            inydbcont = Inydbcont;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<PACIENTE>>> get()
+        {
+            return await inydbcont.Pacientes.ToListAsync();
+
+
+        }
+        [HttpGet("{ID:int}")]
+        public async Task<ActionResult<PACIENTE>> get(int ID)
+        {
+
+            var paciente = await inydbcont.Pacientes.Where(p => p.ID == ID).FirstOrDefaultAsync();
+            if (paciente == null)
+            {
+                return NotFound($"No existe la especialidad de ID={ID}");
+            }
+            return paciente;
+
+            //[HttpGet("PacientePorNombre/{nombre}")]
+            //public async Task<ActionResult<PACIENTE>> get(string nombre)
+            //var paciente = await inydbcont.Pacientes.Where(p => p.nombre == nombre).FirstOrDefaultAsync();
+            //if (paciente == null)
+            //{
+            //   return NotFound($"No existe la especialidad de ID={ID}");
+        }
+        //return paciente;
+        [HttpPost]  
+        public async Task<ActionResult<PACIENTE>> post(PACIENTE paciente)
+        {
+            await inydbcont.Pacientes.AddAsync(paciente);
+            return paciente;
+        }
+    }
+}   
--- HistoriaClinicaBD/Data/Entidades/PACIENTE.cs
+++ HistoriaClinicaBD/Data/Entidades/PACIENTE.cs
@@ -1,35 +1,53 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace HistoriaClinicaBD.Data.Entidades
 {
+    [Index(nameof(DNI), Name = "Dnipaciente-uq", IsUnique = true)]
+
     public class PACIENTE : EntytyBase
     {
         [Required(ErrorMessage = " Campo obligatorio")]
-        [MaxLength(10, ErrorMessage = " El nombre, es obligatorio ")]
+        [MaxLength(10, ErrorMessage = " El nombre,no puede superar los {1} caracteres  ")]
 
         public string nombre{ get; set; }
 
         [Required(ErrorMessage = " Campo obligatorio")]
-        [MaxLength(10, ErrorMessage = " El nombre, es obligatorio ")]
+        [MaxLength(10, ErrorMessage = " El nombre, no puede superar los {1} caracteres ")]
 
         public string Apellido { get; set; }
 
         [Required(ErrorMessage = " Campo obligatorio")]
-        [MaxLength(10, ErrorMessage = " El nombre, es obligatorio ")]
+        [MaxLength(10, ErrorMessage = " El nombre, no puede superar los {1} caracteres ")]
         public string DNI { get; set; }
 
         [Required(ErrorMessage = " Campo obligatorio")]
-        [MaxLength(10, ErrorMessage = " El nombre, es obligatorio ")]
+        public int edad { get; set; }
+
+        [Required(ErrorMessage = " Campo obligatorio")]
+        [MaxLength(10, ErrorMessage = " El nombre, no puede superar los {1} caracteres ")]
         public string Direccion { get; set; }
 
         [Required(ErrorMessage = " Campo obligatorio")]
         
         public int telefono { get; set; }
-        // esta finado si o no
+
+        [Required(ErrorMessage = " Campo obligatorio")]
+        [MaxLength(10, ErrorMessage = " El nombre, no puede superar los {1} caracteres ")]
+
+        public string EstadoCivil { get; set; }
+
+        [Required(ErrorMessage = " Campo obligatorio")]
+        [MaxLength(2, ErrorMessage = " El nombre, no puede superar los {1} caracteres ")]
+
+        public string hijos { get; set; }
+
+
     }
 }
